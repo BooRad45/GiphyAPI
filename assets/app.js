7739f2df027715c9046d7348a2b5a4d3e9d36611
Brendan Rogers <brendan@utexas.edu>
2017-03-27 00:00:15 -0500

    still working with javascript.

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,31 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-// Adding click event listener to TV Show button
-    $("#tvShow-input").on("click", function() {
-      // Grabbing and storing the value from the button
-      var tvShow = $(this).attr("tvShow-input");
+    // Adding click event listener to TV Show button
+    $("#addtvShow").on("click", function() {
+        // Grabbing and storing the value from the field
+        var tvShow = $("#tvShow-input").val();
+        console.log(tvShow);
 
-      // Constructing a queryURL using the tv show name
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        tvShow + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        // Constructing a queryURL using the tv show name
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+            tvShow + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         // Performing an AJAX request with the queryURL
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        // After data comes back from the request
-        .done(function(response) {
-          console.log(queryURL);
-          console.log(response);
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            // After data comes back from the request
+            .done(function(response) {
+                console.log(queryURL);
+                console.log(response);
 
-          var tvShowArray = [];
+                // var tvShowArray = [];
 
-      }) //closes function for ajax call
+            }); //closes function for ajax call
 
-  	}); // closes event listener function for tvshowinput
+    }); // closes event listener function for tvshowinput
 
- }); // closes document.ready function
+}); // closes document.ready function
 
