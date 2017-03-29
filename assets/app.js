e23e95d24ec0a71f4f4c59203099aa652541fb6f
Brendan Rogers <brendan@utexas.edu>
2017-03-28 23:31:53 -0500

    forgot to add code to stop and animate images. Added this commit

--- assets/app.js
+++ assets/app.js
@@ -60,22 +60,45 @@ $(document).ready(function() {
             var p = $("<p>").text("Rating: " + results[i].rating);
 
             // Creating and storing an image tag
-            var tvShowImg = $("<img>");
+            var tvShowImg = $("<img>").addClass ("gif");
+
             // Setting the src attribute of the image to a property pulled off the result item
             tvShowImg.attr("src", results[i].images.fixed_height.url);
+            tvShowImg.attr("data-animate", results[i].images.fixed_height.url);
+            tvShowImg.attr("data-still", results[i].images.fixed_height_still.url);
+            tvShowImg.attr("data-state", "animate");
 
-            // Appending the paragraph and image tag to the tvS
+           
+
+            // Appending the paragraph and image tag 
             tvShowDiv.append(p);
             tvShowDiv.append(tvShowImg);
 
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+            // Prepending the div to the HTML page in the "#gifs-appear-here" div
             $("#gifs-appear-here").prepend(tvShowDiv);
           }
-            });
-         });              
-           
+
+         
+    $(".gif").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "animate") {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      } else {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      }
     });
- }); 
+            });
+         });                     
+    });
+
+  });
+  
 
 
 
